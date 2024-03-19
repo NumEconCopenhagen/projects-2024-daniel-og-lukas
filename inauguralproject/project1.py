d597ea01ca62c02815133aee44dcf2f74a6466d7
twj936 <twj936@alumni.ku.dk>
2024-03-19 12:20:01 +0100

    xv

--- inauguralproject/project1.py
+++ inauguralproject/project1.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({"axes.grid":True,"grid.color":"black","grid.alpha":"0.25","grid.linestyle":"--"})
 plt.rcParams.update({'font.size': 14})
 
-%load_ext autoreload
-%autoreload 2
 
 from ExchangeEconomy import ExchangeEconomyClass
 
